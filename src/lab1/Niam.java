3987ba621a643401de422115c364a0e614130352
Ella Saakyan <esaakyan03@gmail.com>
2023-02-08 11:42:27 +0300

    first commit

--- src/lab1/Niam.java
+++ src/lab1/Niam.java
@@ -2,7 +2,7 @@ package lab1;
 
 public class Niam {
     public static void main(String[] args) {
-        int a = 10;
+        int a = 20;
         int b = 20;
         System.out.println("Сумма двух чисела = " + (a + b));
 

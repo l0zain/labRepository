eed4fe765ab2bf1c4ecb78858d6e684ff2218fbd
Ella Saakyan <esaakyan03@gmail.com>
2023-02-14 16:58:05 +0300

    remake-lab2

--- src/lab2/Primes.java
+++ src/lab2/Primes.java
@@ -8,12 +8,11 @@ public class Primes {
 
     }
     public static boolean isPrime(int n) {
-        int k = 0;
         for(int i = 2; i < n; i ++){
             if (n % i == 0) {
-                k++;
+                return false;
             }
         }
-        return k == 0;
+        return true;
     }
 }
